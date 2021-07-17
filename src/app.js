2c5a018ba49aaf6d3a9f71a31dbcdba94b03e73b
Yevhenbk <balagutrak.jenia@gmail.com>
2021-07-17 15:55:06 +0000

    un mensaje aquí

--- src/app.js
+++ src/app.js
@@ -5,7 +5,32 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.getElementById("button").addEventListener("click", () => {
+    document.getElementById("myDomain").innerHTML = theDomain();
+  });
 };
+
+let firstElement = ["the", "our"];
+let secondElement = ["great", "big"];
+let thirdElement = ["jogger", "racoon"];
+let extentions = [".com", ".net", ".us", ".io"];
+
+function theDomain() {
+  let result = [];
+  for (let i = 0; i < firstElement.length; i++) {
+    for (let j = 0; j < secondElement.length; j++) {
+      for (let h = 0; h < thirdElement.length; h++) {
+        for (let g = 0; g < extentions.length; g++) {
+          result.push(
+            firstElement[Math.floor(Math.random() * firstElement.length)] +
+              secondElement[Math.floor(Math.random() * secondElement.length)] +
+              thirdElement[Math.floor(Math.random() * thirdElement.length)] +
+              extentions[Math.floor(Math.random() * extentions.length)]
+          );
+          return result;
+        }
+      }
+    }
+  }
+}
